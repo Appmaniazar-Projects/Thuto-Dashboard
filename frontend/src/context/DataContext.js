53a07d01788496d3a77161dc4cc3e9978bbc6467
CrazyChips34 <tiffanypietersen234@gmail.com>
2025-10-01 10:41:30 +0200

    fix imports

--- frontend/src/context/DataContext.js
+++ frontend/src/context/DataContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { fetchAllStudents } from '../services/api';
+import api from '../services/api';
 
 const DataContext = createContext();
 
