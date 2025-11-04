1bd447a2c62b2fcfffd41aa92c78f4f428df3cb8
Appmaniazar <andrewncama@gmail.com>
2025-11-04 11:52:19 +0200

    Align to the center

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -185,7 +185,7 @@ const Login = () => {
       component="main" 
       maxWidth="xs"
       sx={{
-        minHeight: '100vh',
+        minHeight: '100vh',  
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center'
